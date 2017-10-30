ed02bb76617bc4408c4d53befe1de83cbfdaf913
handleyrr <rachel.handley2591@gmail.com>
2017-10-30 02:46:13 -0400

    Created Forms Database

---
+++ MissionImpossible/MissionImpossible/Models/Form.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MissionImpossible.Models
+{
+    public class Form
+    {
+        public int Id { get; set;}
+        public string Form_name { get; set; }
+        public string Description { get; set; }
+        public Type Type { get; set; }
+    }
+
+    public enum Type
+    {
+        Event = 1, 
+        Volunteer, 
+        Job
+    }
+}
--- MissionImpossible/MissionImpossible/Models/IdentityModels.cs
+++ MissionImpossible/MissionImpossible/Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace MissionImpossible.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+
+        public DbSet<Form>Forms { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
