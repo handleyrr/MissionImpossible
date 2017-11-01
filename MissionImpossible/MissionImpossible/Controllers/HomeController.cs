0c308be1b97ff1ed32038981a644521f0e10d303
handleyrr <rachel.handley2591@gmail.com>
2017-11-01 14:27:13 -0400

    Fixed the Database. You can now Login and create an account

--- MissionImpossible/MissionImpossible/Controllers/HomeController.cs
+++ MissionImpossible/MissionImpossible/Controllers/HomeController.cs
@@ -10,6 +10,27 @@ namespace MissionImpossible.Controllers
     {
         public ActionResult Index()
         {
+            if (System.Web.HttpContext.Current.User.Identity.IsAuthenticated)// checks to see if user is logged in
+            {
+                if (User.IsInRole("Admin"))
+                {
+                    return RedirectToAction("Admin", "Home"); // if user is an admin, take them to the admin page
+                }
+                if (User.IsInRole("User")) // if user is standard user, go to the user page
+                {
+                    return RedirectToAction("UserAccount", "Home");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Admin()
+        {
+            return View();
+        }
+
+        public ActionResult UserAccount()
+        {
             return View();
         }
 
